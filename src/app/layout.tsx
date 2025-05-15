50712574f2d49f8b239cd927dc38d984b7875581
Raynold Gyasi <speedyray2ray@gmail.com>
2025-05-15 14:42:14 -0400

    added files

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+
+import type { Metadata } from "next";
+import "./globals.css";
+import { ReactNode } from "react";
+import {Providers} from "./providers";
+
+
+export const metadata: Metadata = {
+  title: "TSender",
+  
+};
+
+export default function RootLayout( props: {children: ReactNode}) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>
+        {props.children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
