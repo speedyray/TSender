5f809f95a3b69f49d40e5b075bd1b3f158c83ce2
Raynold Gyasi <speedyray2ray@gmail.com>
2025-05-23 19:34:29 -0400

    Added tests

--- test/playwright/basic.spec.ts
+++ test/playwright/basic.spec.ts
@@ -1,4 +1,11 @@
-import { test, expect } from '@playwright/test';
+
+import basicSetup from '../wallet-setup/basic.setup';
+import { testWithSynpress } from '@synthetixio/synpress';
+import {MetaMask, metaMaskFixtures} from '@synthetixio/synpress/playwright';
+
+const test = testWithSynpress(metaMaskFixtures(basicSetup))
+const {expect} = test
+
 
 test('has title', async ({ page }) => {
   await page.goto('/');
@@ -7,4 +14,35 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/TSender/);
 });
 
+test("should show the airdropform when connected, otherwise, not", async ({ page, 
+  context, metamaskPage, extensionId }) =>{
+  await page.goto('/');
+  //check we see, please connect wallet
+  await expect(page.getByText('Please connect')).toBeVisible();
+
+  const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword, extensionId)
+  await page.getByTestId('rk-connect-button').click()
+  await page.getByTestId('rk-wallet-option-io.metamask').waitFor({
+    state: 'visible',
+    timeout: 30000,
+  })
+
+  await page.getByTestId('rk-wallet-option-io.metamask').click()
+  await metamask.connectToDapp() 
+  const customNetwork = {
+    name: 'Anvil',
+    rpcUrl: 'http://127.0.0.1:8545',
+    chainId: 31337,
+    symbol: 'ETH'
+  }
+  await metamask.addNetwork(customNetwork)
+
+  await expect(page.getByText('Token Address')).toBeVisible();
+
+  // await page.getByRole('textbox', { name: '0x', exact: true }).waitFor({
+  //   state: 'visible',
+  //   timeout: 30000
+  // });
+  
+})
 
--- test/wallet-setup/basic.setup.ts
+++ test/wallet-setup/basic.setup.ts
@@ -2,7 +2,7 @@ import { defineWalletSetup } from '@synthetixio/synpress'
 import { MetaMask } from '@synthetixio/synpress/playwright'
 
 const SEED_PHRASE = 'test test test test test test test test test test test junk'
-const PASSWORD = 'SynpressIsAwesomeNow!!!'
+const PASSWORD = 'Tester@1234'
 
 export default defineWalletSetup(PASSWORD, async (context, walletPage) => {
     const metamask = new MetaMask(context, walletPage, PASSWORD)
